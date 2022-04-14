a0ce8bcd48d1b4f3d0682e453f7c3a2378d038e5
Luiz Felipe Bari <luizfelipebari@gmail.com>
2022-04-14 01:30:42 -0300

    Subindo o Projeto01 V1

---
+++ Assets/JS/script.js
@@ -0,0 +1,23 @@
+const buttonclick = document.querySelector("#button");
+const img = document.querySelector("#imagem");
+const pEvoluiu = document.querySelector("#pEvoluiu");
+let buttonClickValue = 0;
+
+const imgList = [
+  "./Assets/imagens/Charmander 1.png",
+  "./Assets/imagens/Charmeleon.png",
+  "./Assets/imagens/Charizard Esnobe.png",
+];
+
+buttonclick.addEventListener("click", () => {
+  if (img.attributes.src.nodeValue == imgList[0]) {
+    pEvoluiu.innerText = "O Charmander evoluiu para Charmeleon";
+    img.setAttribute("src", `${imgList[1]}`);
+  } else if (img.attributes.src.nodeValue == imgList[1]) {
+    pEvoluiu.innerText = "O Charmeleon evoluiu para Charizard";
+    img.setAttribute("src", `${imgList[2]}`);
+  } else {
+    pEvoluiu.innerText = "Algo de errado aconteceu, o Charizard involuiu para Charmander";
+    img.setAttribute("src", `${imgList[0]}`);
+  }
+});
